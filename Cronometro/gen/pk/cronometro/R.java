526739326a37516a2f46506ae81694f590b49d32
egmn472 <egmn472@gmail.com>
2013-04-29 20:39:57 -0500

    Adicion de table layout y linearlayout para iniciar la capa de presentacion

--- Cronometro/gen/pk/cronometro/R.java
+++ Cronometro/gen/pk/cronometro/R.java
@@ -15,7 +15,11 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int btnCentesimas=0x7f070003;
+        public static final int btnHoras=0x7f070000;
+        public static final int btnMinutos=0x7f070001;
+        public static final int btnSegundos=0x7f070002;
+        public static final int menu_settings=0x7f070004;
     }
     public static final class layout {
         public static final int frm_cronometro=0x7f030000;
@@ -25,9 +29,13 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_frm__cronometro=0x7f040003;
+        public static final int btnLargeSize=0x7f040006;
+        public static final int btnNormalSize=0x7f040005;
+        public static final int btnSmallSize=0x7f040004;
+        public static final int btnValorInicial=0x7f040003;
+        public static final int btnXLargeSize=0x7f040007;
+        public static final int menu_settings=0x7f040001;
+        public static final int title_activity_frm__cronometro=0x7f040002;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
